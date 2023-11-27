23d14c36a958e5e6454ca94d647973362e3bf06c
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-27 19:36:19 +0100

    Escape eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  const task = false; // eslint-disable-line no-unused-vars
-  const task2 = true; // eslint-disable-line no-unused-vars
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-
   return [task, task2];
 }
