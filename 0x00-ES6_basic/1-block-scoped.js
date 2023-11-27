f8f11f38a3077451590fcd38643d0951dc8b5bfa
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-27 17:39:00 +0100

    Convert let to const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false; // eslint-disable-line no-unused-vars
+  const task2 = true; // eslint-disable-line no-unused-vars
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
