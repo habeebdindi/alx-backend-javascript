b96c28e6b6ea6bd8065af0e3491b63e42e5553a2
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-28 22:06:58 +0100

    Change import file path

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { uploadPhoto, createUser } from '../utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([
