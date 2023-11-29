97787b8aefd06be934ab109dad08280df9cb93cd
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 17:09:45 +0100

    Fix eslint error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,18 @@
+/* eslint-disable  */
 import { uploadPhoto, createUser } from './utils.js';
 
 export default async function asyncUploadUser() {
-    try {
-	const userPic = await uploadPhoto();
-	const user = await createUser();
-	return {
-	    photo: userPic,
-	    user: user,
-	};
-    } catch (error) {
-	return {
-	    photo: null,
-	    user: null,
-	};
-    }
+  try {
+    const userPic = await uploadPhoto();
+    const user = await createUser();
+    return {
+    photo: userPic,
+    user,
+    };
+  } catch (error) {
+    return {
+    photo: null,
+    user: null,
+    };
+  }
 }
