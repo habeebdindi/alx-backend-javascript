5bc78ecf5d45debb14ef2017592b6a67f9dedcdd
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 15:31:42 +0100

    Use the appropriate return formaat

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,12 +3,17 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    let retArray = []
+    const retArray = []
     return Promise.allSettled([
 	signUpUser(firstName, lastName),
 	uploadPhoto(fileName)
     ]).then((messages) => {
-	retArray.push(messages);
-	return retArray.flat();
+	messages.map((message) => {
+	    retArray.push({
+		status: message.status,
+		value: message.status === "rejected" ? message.reason.toString() : message.value
+	    });
+	});
+	return retArray;
     });
 };
