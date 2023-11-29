3d6c7137b475108feeae0e308b089bcc72878d0f
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 15:06:43 +0100

    Return an array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,12 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
+    let retArray = []
+    Promise.allSettled([
 	signUpUser(firstName, lastName),
 	uploadPhoto(fileName)
     ]).then((messages) => {
-	return messages;
+	retArray.push(messages);
+	return retArray.flat();
     });
 };
