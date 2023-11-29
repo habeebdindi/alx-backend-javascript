3da524be2956223f22f8c55a0fdc32adcd81ec9b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 14:13:45 +0100

    Add a .then

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,10 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+    return Promise.allSettled([
+	uploadPhoto(fileName),
+	signUpUser(firstName, lastName)
+    ]).then((messages) => {
+	console.log(messages);
+    });
 }
