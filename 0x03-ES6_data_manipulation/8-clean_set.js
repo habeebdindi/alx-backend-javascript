7c7ff5c97d4984db72a02c06885b2c46e1a9c132
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-06 01:46:58 +0100

    Correct if condition

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 function cleanSet(set, startString) {
     let result = '';
     set.forEach(value => {
-        if (value.startsWith(startString)) {
+        if (startString && value.startsWith(startString)) {
             const remaining = value.substring(startString.length);
             result += remaining + '-';
         }
