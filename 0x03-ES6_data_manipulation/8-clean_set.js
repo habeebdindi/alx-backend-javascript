c8dfb45939613547e3648a69e0ed59e6a9cf2a74
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-06 02:03:29 +0100

    Try .length method

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
   set.forEach((value) => {
-    if (startString && value.startsWith(startString)) {
+    if (startString.length && value.startsWith(startString)) {
       // const remaining = value.substring(startString.length);
       const remaining = value.split(startString)[1];
       result.push(remaining);
