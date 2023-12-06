4c88eb27493c91954622606627ac07405cc8358b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-06 01:59:52 +0100

    Try alternative method

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,11 @@
-function cleanSet(set, startString) {
-    let result = '';
-    set.forEach(value => {
-        if (startString && value.startsWith(startString)) {
-            const remaining = value.substring(startString.length);
-            result += remaining + '-';
-        }
-    });
-    return result.slice(0, -1);
+export default function cleanSet(set, startString) {
+  const result = [];
+  set.forEach((value) => {
+    if (startString && value.startsWith(startString)) {
+      // const remaining = value.substring(startString.length);
+      const remaining = value.split(startString)[1];
+      result.push(remaining);
+    }
+  });
+  return result.join('-');
 }
-
-export default cleanSet;
