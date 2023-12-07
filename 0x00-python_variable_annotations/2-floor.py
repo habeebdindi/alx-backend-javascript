272939585ab86bec95385fd10092b468d37675dd
Habeeb Dindi <76703071+habeebdindi@users.noreply.github.com>
2023-12-08 00:53:29 +0100

    Delete 0x00-python_variable_annotations directory

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""This module contains a function the returns a floor of a float"""
-
-import math
-
-
-def floor(n: float) -> int:
-    """This function returns a floor of the argument"""
-    return math.floor(n)
--- 0x00-python_variable_annotations/README.md
+++
@@ -1,3 +0,0 @@
-## This directory contains basic Python files performing simple actions
-
-### Main concepts used here are python's typing feature.
